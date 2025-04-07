225a70291c8d4afb9dae3e3887ada02c6fe0e753
alumno <alumno@pc-candelap.uacj.mx>
2025-04-07 09:53:47 -0600

    [AGREGA] tamaño a las fotos y un preview

--- Redes_sociales/Redes_sociales/Redes_sociales/UI/dragon_ball/pantalla_personajes.swift
+++ Redes_sociales/Redes_sociales/Redes_sociales/UI/dragon_ball/pantalla_personajes.swift
@@ -11,14 +11,36 @@ struct PantallaPersonajes: View {
     @Environment(ControladorAplicacion.self) var controlador
     var body: some View {
         if(controlador.paginaResultados != nil){
-            ScrollView{
-                LazyVStack{
-                    ForEach(controlador.paginaResultados!.items){ personaje in
-                        Text("El personaje es \(personaje.name)")
-                        AsyncImage(url: URL(string: personaje.image))
+            NavigationStack{
+                ScrollView{
+                    LazyVStack{
+                        ForEach(controlador.paginaResultados!.items){ personaje in
+                            NavigationLink{
+                                PreviewPersonaje()
+                            }label:{
+                                AsyncImage(url: URL(string: personaje.image)){imagen in
+                                    imagen
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(width: 50)
+                                        .clipShape(Rectangle())
+                                } placeholder: {
+                                    ProgressView()
+                                        .padding()
+                                        .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
+                                        .background(Color.red)
+                                        .clipShape(Circle())
+                                }
+                                Text("\(personaje.name)")
+                                
+                                    
+                            }
+                            
+                        }
                     }
                 }
             }
+            
             
         }
         
---
+++ Redes_sociales/Redes_sociales/Redes_sociales/UI/dragon_ball/preview_personaje.swift
@@ -0,0 +1,18 @@
+//
+//  preview_personaje.swift
+//  Redes_sociales
+//
+//  Created by alumno on 4/7/25.
+//
+
+import SwiftUI
+
+struct PreviewPersonaje: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PreviewPersonaje()
+}
